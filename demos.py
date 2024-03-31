445af88fb8c3f9b1dd811c974b016f514b9df498
Emil Højlund Larsen <hojlund123@users.noreply.github.com>
2024-03-31 17:08:51 +0200

    Reversibel results url

--- demos.py
+++ demos.py
@@ -92,7 +92,7 @@ def _monthNameToNumber(monthName: str):
     return datetime.datetime.strptime(monthName, "%B").month
 
 def get_results():
-    results = get_parsed_page("https://www.hltv.org/results?offset=200")
+    results = get_parsed_page("https://www.hltv.org/results")
 
     results_list = []
 
